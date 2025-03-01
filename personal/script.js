56995f499ffb47a5fd30c0e267fedd5cf79382bd
aanm1 <136671897+aanm1@users.noreply.github.com>
2025-03-01 20:48:05 +0000

    l

--- personal/script.js
+++ personal/script.js
@@ -1,7 +1,15 @@
-function start() {
-    let song = new Audio("dan.mp3");  // Create a new audio object
-    song.play();  // Play the audio file
+document.querySelector("img").addEventListener("click",play);
+
+
+function play(){
+    let go = 1;
+    alert (go);
 }
 
-// Execute the function when the window has fully loaded
-window.onload = start();
+
+function start() {
+    let song = new Audio("dan.mp3"); 
+    song.play(); 
+}
+
+window.onload = start;
