bd74a8c17121c0978884dc9fa8a0117658ddbbad
aanm1 <136671897+aanm1@users.noreply.github.com>
2025-05-06 08:50:36 +0000

    for script

--- personal/script.js
+++ personal/script.js
@@ -1,11 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000; // or any port you prefer
+const port = 3000;
 
-app.use(bodyParser.json()); // to parse JSON bodies
-app.use(bodyParser.urlencoded({ extended: true })); // to parse URL-encoded bodies
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Server is on and listening at http://localhost:${port}`);
